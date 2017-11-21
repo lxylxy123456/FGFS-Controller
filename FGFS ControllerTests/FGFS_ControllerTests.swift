e33465dee7530f71552da278219eb78f5ab47400
lxylxy123456 <13691419520@163.com>
2017-11-21 09:42:11 -0800

    Add License AGPL

--- FGFS ControllerTests/FGFS_ControllerTests.swift
+++ FGFS ControllerTests/FGFS_ControllerTests.swift
@@ -1,10 +1,20 @@
-//
-//  FGFS_ControllerTests.swift
-//  FGFS ControllerTests
-//
-//  Created by 李宵逸 on 2017/11/13.
-//  Copyright © 2017年 李宵逸. All rights reserved.
-//
+//  
+//  FGFS-Controller - Using an iOS Device to Control FGFS
+//  Copyright (C) 2017  lxylxy123456
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Affero General Public License for more details.
+//  
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//  
 
 import XCTest
 @testable import FGFS_Controller
